30237345944c1e4fb955443db5abc9ef915194a8
Annie Truex <artruex@gmail.com>
2019-08-08 20:12:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
-# Add your code here
+class Dog 
+  
+  @@all = []
+  
+  def initialize 
+
+    @@all << self 
+  end
+  
+  def self.print_all 
+    @@all.each do |dog|
+      
+  end
+  
+  def self.all 
+    @@all 
+  end
+  
+end
